e89ffd9abb63a19de582dc9094aa6e29162875a6
IM too cool <thomas.hendrickson@tufts.edu>
2016-10-22 19:07:11 -0400

    added command processing\input handling

--- comp111-shell/shell.c
+++ comp111-shell/shell.c
@@ -1,13 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-//test comment
+#include <string.h>
 
 
-
-
-
-
+//shell prompt
 #define PROMPT "$> "
 /*
 void quitHandler(   );
@@ -18,6 +15,9 @@ void runInBackground(char*[] args);
 */
 //interactive mode
 void interactive();
+
+//handle line
+void handle_line(char *buf, ssize_t len);
 
 int main(int argc, char* argv[]){
 
@@ -33,19 +33,19 @@ int main(int argc, char* argv[]){
                 exit(EXIT_FAILURE);
         }
 }
-/*
-void interactive(){
-	char* env = //();
 
-	while(true){
-		printf("%s", "prompt: ");
-		batch(getline());
-		printf("%s", "\n");
-	}
-}
-*/
+
 
 //https://www.gnu.org/software/libc/manual/html_node/Line-Input.html
+/*
+ * interactive mode
+ * purpose: runs the command shell in interactive mode until 
+ *          CTRL-D (EOF) is signaled 
+ * arguments: NONE
+ * returns: NONE
+ *
+ * NOTE: should not return (program will exit from this function)
+ */
 void interactive() 
 {
         ssize_t bytes_read;
@@ -53,15 +53,57 @@ void interactive()
         char *buf = NULL; 
 
         while (true) {
+                //check for EOF
+                if (feof(stdin)) {
+                        printf("\n");
+                        free(buf);
+                        exit(EXIT_SUCCESS);
+                        //TODO
+                        //JUST CALL EXIT FUNCTION IMPLEMENTED LATER
+                        //with memory cleaned up
+                }
                 printf("%s", PROMPT);
-                //read from command prompt
                 bytes_read = getline(&buf, &len, stdin);             
-                printf("%s", "\n");
-                printf("%s", buf);
-                printf("%s", "\n");
+                handle_line(buf, bytes_read);
         }
 }
 
+
+void handle_line(char *buf, ssize_t len)
+{
+        const char delim[2] = ";";
+        char *token;
+
+        //get first 'token'
+        token = strtok(buf, delim);
+         //if(args[length(args) - 1]  == "&") runInBackground(args);
+        while (token != NULL) {
+                //check command, if not in list then
+                //attempt to run as executable
+                if (strncmp(token, "cd", 2) == 0) {
+                        printf("%s\n", token);
+                } else if (strncmp(token, "clr", 3) == 0) {
+                        printf("clr\n");
+                } else if (strncmp(token, "dir", 3) == 0) {
+                        printf("dir\n");
+                } else if (strncmp(token, "environ", 7) == 0) {
+                        printf("environ\n");
+                } else if (strncmp(token, "echo", 4) == 0) {
+                        printf("echo %s\n", token);
+                } else if (strncmp(token, "help", 4) == 0) {
+                        printf("help\n");
+                } else if (strncmp(token, "pause", 5) == 0) {
+                        printf("pause\n");
+                } else if (strncmp(token, "quit", 4) == 0) {
+                        printf("quit\n");
+                } else if (strncmp(token, ";", 1) != 0 &&
+                           strncmp(token, "\n", 1) != 0) {
+                        printf("run: %s\n", token);
+                        //run the token as an executable
+                }
+                token = strtok(NULL, delim);
+        }
+}
 
 /*
 void quitHandler(   ){
